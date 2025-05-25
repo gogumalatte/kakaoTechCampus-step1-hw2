1d1fbd012127a21084956fec736c14d6818b6b8a
KIYEONG <rldud1237@naver.com>
2025-05-26 01:12:58 +0900

    feat: implement GET /schedules endpoint to retrieve all schedules

--- Lv0-2/src/main/java/com/kakao/schedule/controller/ScheduleController.java
+++ Lv0-2/src/main/java/com/kakao/schedule/controller/ScheduleController.java
@@ -1,9 +1,12 @@
 package com.kakao.schedule.controller;
 
 import com.kakao.schedule.dto.ScheduleRequest;
+import com.kakao.schedule.dto.ScheduleResponse;
 import com.kakao.schedule.service.ScheduleService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/schedules")
@@ -19,5 +22,11 @@ public class ScheduleController {
   public ResponseEntity<Void> createSchedule(@RequestBody ScheduleRequest request) {
     scheduleService.createSchedule(request);
     return ResponseEntity.ok().build();
+  }
+
+  @GetMapping
+  public ResponseEntity<List<ScheduleResponse>> getAllSchedules() {
+    List<ScheduleResponse> schedules = scheduleService.getAllSchedules();
+    return ResponseEntity.ok(schedules);
   }
 }
